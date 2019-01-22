89ae9340d11f6245ff5a717113652bf8e8733bb5
Chen Yanyu <530379451@qq.com>
2019-01-23 00:27:35 +0800

    没事的

--- src/cn/java/mapper/PreferMapper.java
+++ src/cn/java/mapper/PreferMapper.java
@@ -1,12 +1,13 @@
 package cn.java.mapper;
 
-import cn.java.entity.Prefer;
+// import cn.java.entity.Prefer;
 import cn.java.entity.PreferKey;
 import org.springframework.stereotype.Repository;
 
 /**
  * PreferMapper继承基类
  */
-@Repository
-public interface PreferMapper extends MyBatisBaseDao<Prefer, PreferKey> {
-}
+// 有点错
+// @Repository
+// public interface PreferMapper extends MyBatisBaseDao<Prefer, PreferKey> {
+// }
