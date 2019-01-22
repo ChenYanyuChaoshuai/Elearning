e742fa9e69ed87c197a1602e8bcd18f432a07caa
Chen Yanyu <530379451@qq.com>
2019-01-22 14:09:12 +0800

    彻底修改版的初始工程，我觉得可以merge

---
+++ out/artifacts/Zhiku_war_exploded/index.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Think
+  Date: 2019/1/21
+  Time: 15:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>智库初始工程</title>
+  </head>
+  <h1>欢迎来到智库初始工程</h1>
+  </body>
+</html>
---
+++ src/cn/java/controller/test_hello.java
@@ -0,0 +1,14 @@
+package cn.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class test_hello {
+    @RequestMapping(value="/test_hello")
+    public String inputProduct(Model model){
+        
+        return "front/test-helloword.jsp";
+    }
+}
